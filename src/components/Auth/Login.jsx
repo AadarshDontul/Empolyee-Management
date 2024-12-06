e917e1e9f2daa0caea8f08bb73d67a5e6f3b0105
Aadarsh-D <aadarshdontul03@gmail.com>
2024-12-06 20:00:52 +0530

    completed html and tailwind css code for login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-red-700'>
+        <div className='border-2 border-emerald-600 rounded-xl p-20'>
           <form className='flex flex-col items-center justify-center' action="">
-            <input type="email" placeholder='Enter your email'/>
-            <input type="password" placeholder='Enter password'/>
+            <input required className='outline-none bg-transparent border-2 border-emerald-600 text-xl rounded-full py-3 px-5  text-white placeholder:text-gray-300' type="email" placeholder='Enter your email'/>
+            <input required className='outline-none bg-transparent border-2 border-emerald-600 text-xl rounded-full py-3 px-5 mt-3 text-white placeholder:text-gray-300' type="password" placeholder='Enter password'/>
+            <button className='w-full outline-none border-none font-semibold bg-emerald-600 text-xl rounded-full py-3 px-5 mt-5 text-white placeholder:text-white'>Log in</button>
           </form>
         </div>
     </div>
